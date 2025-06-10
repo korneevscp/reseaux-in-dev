da8c3ce64759fa6f31375e63a65cb9e64c507835
korneevscp <vladimirivanof2024@gmail.com>
2025-06-10 11:37:16 +0200

    sup

--- web1-2/bouton/script.js
+++
@@ -1,5 +0,0 @@
-var temp = document.getElementById('temp')
-var number=0
-if(number===0){temp.innerText= "warning temp"}
-else{document.getElementById('temp2').style.display="none";
-}
--- web1-2/includes/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=social_network;charset=utf8mb4", "root", "");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
-?>
